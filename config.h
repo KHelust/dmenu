9476871132f7e03581f447bd2f58762bde128cec
Héctor Estrada <5913563-KHelust@users.noreply.gitlab.com>
2020-07-26 10:53:53 -0500

    improving seconadry colors

--- config.h
+++ config.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ab47bc", "#212121" },
-	[SchemeSel] = { "#40c4ff", "#212121" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#ea80fc", "#212121" },
+	[SchemeSel] = { "#29b6f6", "#424242" },
+	[SchemeOut] = { "#212121", "#80deea" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
