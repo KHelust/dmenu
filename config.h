b9532cb511c2686d73d89543ae03444d27f6e34d
Héctor Estrada <5913563-KHelust@users.noreply.gitlab.com>
2021-09-15 00:49:21 -0500

    Cambiar el tema para tener acentos en amarillo

--- config.h
+++ config.h
@@ -1,17 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;       /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;       /* -b  option; if 0, dmenu appears at bottom */
+static int centered = 0;     /* -c option; centers dmenu on screen */
+static int min_width = 500;  /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"VictorMono Nerd Font:size=12:antialias=true:autohint=true"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#928374", "#192124" },
-	[SchemeSel] = { "#8ec07c", "#192124" },
-	[SchemeOut] = { "#282828", "#9d0006" },
+	[SchemeNorm] = { "#434758", "#192124" },
+	[SchemeSel] = { "#ffcb6b", "#192124" },
+	[SchemeOut] = { "#292d3e", "#f07178" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
